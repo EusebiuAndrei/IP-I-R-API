187ed57aafdb8d3a2bbbb3d96f78c48cdf08d489
limelier <tudor.iacobescu.1@gmail.com>
2020-04-16 14:56:35 +0300

    Finished Reviews service.

--- models/review/validator.js
+++ models/review/validator.js
@@ -1,8 +1,8 @@
 const { Joi } = require('celebrate');
 
 const schema = Joi.object().keys({
-    provider_id: Joi.string().hex().length(24),
-    reviewer_id: Joi.string().hex().length(24),
+    providerId: Joi.string().hex().length(24),
+    reviewerId: Joi.string().hex().length(24),
     score: Joi.number().integer().min(1).max(10),
     description: Joi.string().min(1).max(1000),
 });
--- services/Reviews.js
+++ services/Reviews.js
@@ -1,17 +1,71 @@
+const { ObjectId } = require('mongoose').types;
+const { CastError } = require('mongoose');
+
+const { responseBuilder } = require('../util/responseUtil');
+
 class Reviews {
     constructor({ db }) {
         this.db = db;
     }
 
     async getForProvider(providerId) {
-        const reviews = await this.db.Review.statics.getProviderReviews(
-            providerId,
-        );
-        return reviews.map((review) => review.getObject());
+        try {
+            const dbReviews = await this.db.Review.statics.getProviderReviews(
+                providerId,
+            );
+
+            const reviews = dbReviews.map((review) =>
+                review.getObject(),
+            );
+
+            if (reviews) {
+                const score =
+                    reviews.reduce(
+                        (accumulator, review) =>
+                            accumulator + review.score,
+                    ) / reviews.length;
+
+                return responseBuilder(true, {
+                    score,
+                    reviews,
+                });
+            }
+            return responseBuilder(false, {
+                message:
+                    'No reviews found for the given provider ID.',
+            });
+        } catch (error) {
+            if (error instanceof CastError) {
+                return responseBuilder(false, {
+                    message: 'CastError: ID not valid.',
+                    reason: error.reason.message,
+                });
+            }
+            return responseBuilder(false, { message: error.message });
+        }
     }
 
-    async put() {
-        // ...
+    async post(review) {
+        const dbReviewBody = {
+            provider: new ObjectId(review.providerId),
+            reviewer: new ObjectId(review.reviewerId),
+            score: review.score,
+            description: review.description,
+            timeCreated: new Date(),
+            timeUpdated: new Date(),
+        };
+
+        try {
+            const result = await this.db.Review.create(dbReviewBody);
+
+            return responseBuilder(true, {
+                id: result.id,
+            });
+        } catch (error) {
+            return responseBuilder(false, {
+                message: error.message,
+            });
+        }
     }
 }
 
