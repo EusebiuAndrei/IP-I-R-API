b9778f4937d9f0c76e2160a38e9da5f3bae5f0e3
limelier <tudor.iacobescu.1@gmail.com>
2020-05-04 20:55:59 +0300

    Removed deprecated /api/providers route.

--- api/index.js
+++ api/index.js
@@ -2,10 +2,8 @@ const { Router } = require('express');
 
 const router = Router();
 
-const providerRouter = require('./routes/providers');
 const reviewRouter = require('./routes/reviews');
 
-router.use('/providers', providerRouter);
 router.use('/reviews', reviewRouter);
 
 module.exports = router;
